f0be4e39c440cfbc2a884fce26bf7e05b346ac3c
Ana Beatriz Carvalho <ana.bcarvalho@souunit.com.br>
2022-05-18 21:09:01 -0300

    Terceiro Módulo

---
+++ for.js
@@ -0,0 +1,11 @@
+const numeros = [100, 200, 300, 400, 500, 600];
+
+for (let i = 0; i < numeros.length; i++){
+    console.log(i, numeros[i]);
+}
+
+console.log('------------------------------------')
+
+for(let i = 0; i <= 10; i++){
+    console.log(i);
+}
