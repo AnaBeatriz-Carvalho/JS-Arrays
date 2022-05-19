806e8d09e7a86f832227252d1722c0ad4451dc77
Beatriz Carvalho Oliveira <ana.bcarvalho@souunit.com.br>
2022-05-19 13:30:05 -0300

    Quarto Módulo

---
+++ callbacks.js
@@ -0,0 +1,8 @@
+const nomes = ['Ana', 'Ju', 'Leo', 'Paula'];
+
+// primeiro parâmetro é a função, o segundo pode ser um índice.
+nomes.forEach(ImprimeNomes);
+
+function ImprimeNomes(nome){
+    console.log(nome);
+}
